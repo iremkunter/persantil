03e3f9d8ae83c348a1ebdf9474a784d6c36f7fa1
iremkunter <irem.knter@gmail.com>
2026-09-21 00:19:28 +0300

    Kiz boy 66. ay L degeri duzeltildi (0.0636 -> 1.0636); onbellek surumu v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var C="tt-persantil-v2";
+var C="tt-persantil-v3";
 var F=["./","./index.html","./who.js","./neyzi.js","./manifest.webmanifest",
        "./icon-192.png","./icon-512.png","./icon-180.png"];
 self.addEventListener("install",function(e){
